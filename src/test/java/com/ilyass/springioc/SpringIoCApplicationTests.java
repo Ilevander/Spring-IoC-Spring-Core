97f333daf31172d8473933e06dc993ecb30e04df
dell <dell@DESKTOP-4P8U8AR>
2024-12-23 00:18:09 +0100

    dependencies to start Spring IOC

--- src/test/java/com/ilyass/springioc/SpringIoCApplicationTests.java
+++ src/test/java/com/ilyass/springioc/SpringIoCApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringIoCApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
