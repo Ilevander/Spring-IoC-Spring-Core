7dbd8fe80e806a8db716197a7ec0da75053ddfb6
dell <dell@DESKTOP-4P8U8AR>
2024-12-23 01:45:35 +0100

    Domumenting code (FR): Test methods

--- src/test/java/com/ilyass/springioc/TestService.java
+++ src/test/java/com/ilyass/springioc/TestService.java
@@ -32,5 +32,22 @@ class TestService {
 				() -> Assertions.assertEquals(5d, service.findById(1L).getQuantity(), "First article quantity is not correct"));
 	}
 
-
 }
+
+/*
+    • L’annotation @BeforeAll de JUNIT permet d’exécuter la méthode une seule fois au début (avant toutes les méthodes annotées par @Test).
+
+    • La méthode annotée par @BeforeAll doit être static.
+
+    • L’annotation @AfterAll de JUNIT permet d’exécuter la méthode une seule fois à la fin (après toutes les méthodes annotées par @Test).
+
+    • La méthode annotée par @AfterAll doit être static.
+
+    • Remarquez que nous avons commencé par instancier la classe AnnotationConfigApplicationContext en lui passant en paramètre la classe MainApplication.
+
+    En effet, ceci permettra de créer le conteneur de Spring et d’ajouter à ce dernier toutes les classes se trouvant au niveau du package ma.formations.ioc.
+
+    • La méthode AnnotationConfigApplicationContext.getBean(ServiceImpl.class)  permet de récupérer une instance de la classe ServiceImpl.
+
+    • Remarquez que nous avons utilisé la méthode Assertions.assertAll qui permet d’exécuter plusieurs vérifications.
+ */
