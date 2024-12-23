5b412a3732e01baf855068db60ab230409644418
dell <dell@DESKTOP-4P8U8AR>
2024-12-23 01:43:06 +0100

    Domumenting code (FR): Service Layer

--- src/main/java/com/ilyass/springioc/service/ServiceImpl.java
+++ src/main/java/com/ilyass/springioc/service/ServiceImpl.java
@@ -12,7 +12,14 @@ import java.util.List;
 NB : Spring crée par défaut les Beans sous forme de Singleton (une seule instance).
 Si vous voulez que Spring crée à chaque fois une nouvelle instance du même Bean, il suffit d’annoter votre classe par : @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE).
  */
+
 //@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+
+/*
+    • L’annotation @Service de Spring Context permet à Spring de gérer le Bean annoté par cette dernière.
+
+    • L’annotation @Service est utilisée sur les classes de la couche Service (ou Métier).
+ */
 @Service
 public class ServiceImpl implements IService{
     private IDao dao;
@@ -48,4 +55,19 @@ public class ServiceImpl implements IService{
         return dao.findById(id);
     }
 }
+
+/*
+    • L’annotation @Autowired appliquée sur le setter permet d’injecter le Bean implémentant l’interface IDao. Dans ce cas, Spring IOC passe une instance de la classe implémentant l’interface IDao en paramètre de la méthode setDao(IDao dao) : c’est l’injection par Modificateur.
+    A partir de la version 4.3 de Spring, l’annotation @Autowired est facultative à condition que la classe ait un seul constructeur.
+    Le cas échéant, il faut annoter le setter par cette dernière.
+
+    • Vous constatez que la classe ServiceImpl n’a aucune connaissance de la classe implémentant l’interface IDao.
+    En effet, nous pouvons changer l’implémentation de l’interface IDao sans modifier le code source de la classe ServiceImpl : c’est le principe de couplage faible.
+
+    • Pour que Spring puisse injecter une instance de la classe DaoImpl2 au lieu de la classe DaoImpl1, il suffit d’annoter la classe DaoImpl2 par @Repository. Dans ce cas, il faut supprimer l’annotation @Repository de la classe DaoImpl1.
+
+    • Si vous souhaitez garder l’annotation @Repository sur les deux classes DaoImpl1 et DaoImpl2, vous pouvez utiliser l’annotation @Qualifier comme expliqué ci-après :
+    De cette façon, Spring injectera une instance de la classe DaoImpl2.
+
+ */
 
